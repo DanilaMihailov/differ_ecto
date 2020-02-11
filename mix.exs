dc776da100d18e596924a3a38e686b18c761cef9
Danila Mihailov <danilamihailov@icloud.com>
2020-02-11 14:57:09 +0500

    add license and update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,18 +5,20 @@ defmodule DifferEcto.MixProject do
     [
       app: :differ_ecto,
       version: "0.1.0",
+      source_url: "https://github.com/DanilaMihailov/differ_ecto",
+      homepage_url: "https://github.com/DanilaMihailov/differ_ecto",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      description: description(),
+      package: package(),
       docs: docs()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
@@ -26,6 +28,17 @@ defmodule DifferEcto.MixProject do
       {:ecto_sql, "~> 3.0"},
       # generating documentation (mix docs)
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Ecto.Type for Differ (and some helpers)"
+  end
+
+  defp package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/DanilaMihailov/differ_ecto"}
     ]
   end
 
